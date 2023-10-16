77ce91c66612ba11fa4e9fc25f1ed30de6130703
Ishaq Islam <K2131958@kingston.ac.uk>
2023-10-16 16:23:48 +0100

    Changes

--- Bread TD/Assets/Ishaq's Scripts and Prefabs/Scripts/Bread.cs
+++ Bread TD/Assets/Ishaq's Scripts and Prefabs/Scripts/Bread.cs
@@ -1,22 +1,45 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
 public class Bread : MonoBehaviour
 {
     public float health;
-    public float speed;
+    public float speed = 1.0f;
 
     public GameObject[] bSprites;
     public Transform spawnPoint;
+    public GameObject[] MoveNodes;
+
+    public Transform target;
+
 
     public void SpawnBread()
     {
         foreach (GameObject preSprites in bSprites)
         {
             GameObject EBread = Instantiate(preSprites, spawnPoint.position, Quaternion.identity);
+            bSprites.Append(EBread);
+        }
+    }
+
+    public void BreadShallMove()
+    {
+        foreach (GameObject sprites in bSprites)
+        {
+             foreach (GameObject mNodes in MoveNodes)
+             {
+                for (int i = 0; i < bSprites.Length; i++) 
+                {
+                    Vector3 distance = sprites.transform.position - mNodes.transform.position;
+                    var step = speed * Time.deltaTime;
+                    sprites.transform.position = Vector3.MoveTowards(transform.position, mNodes.transform.position, step);
+                }
+
+             }
         }
     }
     // Start is called before the first frame update
@@ -28,6 +51,6 @@ public class Bread : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        BreadShallMove();
     }
 }
---
+++ Bread TD/Assets/Ishaq's Scripts and Prefabs/Scripts/NodesScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodesScript : MonoBehaviour
+{
+    public GameObject[] moverNodes;
+
+    Bread breadRef;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    { 
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
