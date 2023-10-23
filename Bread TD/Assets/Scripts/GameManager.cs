ce5e0ca355c44ab1b79f139b56e6eab8b130ad69
Shaun Brady <k2129184@kingston.ac.uk>
2023-10-23 17:54:01 +0100

    updated game manager

--- Bread TD/Assets/Scripts/GameManager.cs
+++ Bread TD/Assets/Scripts/GameManager.cs
@@ -1,52 +1,83 @@
+using NUnit.Framework;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     //values needed to be persistant accross scenes
-    static int Score;
+    private static int Score = 0;
 
-    static List<bool> lvlLockList; //just showes if a level is unlocked or not
+    static List<bool> lvlLockList; //list of locked and unlocked waves
 
-    //list of levels
-    
+    //wave list for level
+    List<GameObject> lvlWaves;
+
+    //bread list
+    private List<GameObject> AliveBreads;
 
     //lvl values
     int Lives;
     int lvlScore;
-    int wave;
+    int waveNum;
+    GameObject wave;
     int currentWave;
     int waveDelay;
+
+    int breadCount;
 
     // Start is called before the first frame update
     void Start()
     {
-        Lives = 3; lvlScore = 0; wave = 1;
-        StartWave(wave);
+        //setup lvl defaults
+
+        Lives = 3;
+        lvlScore = 0;
+        waveNum = 1;
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        //check if wave finsihed
+        if (AliveBreads.Count == 0)
+        {
+            //wave Finished start new wave
+            waveNum++;
+            StartWave(waveNum);
+        }
     }
 
-    void LoadLevel()
+    bool LoadLevel(int lvl)
     {
-        //first check if level is locked
+        //first check if level is locked and return false if it is
+        if (IsLevelLocked(lvl)) return false;
 
+        return true;
+    }
+
+    void finishLevel()
+    {
         //need to add lvl score to total score
+        Score += lvlScore;
+
+        //open level finsih Ui
     }
 
-    void StartWave(int waveNum)
+    void StartWave(int WaveNum)
     {
+        wave = lvlWaves[waveNum - 1];
+        //if wave is null then lvl is finished
+        if (wave == null) return;
+
         //delay before wave starts
+        //wave.GetComponent<waveScript>().startDelay;
     }
 
     void FinishWave()
     {
         //make sure all enemies are dead before finishing
+        
     }
 
     bool IsLevelLocked(int level) 
@@ -55,5 +86,10 @@ public class GameManager : MonoBehaviour
         bool lvllocked = lvlLockList[level];
         
         return lvllocked;
+    }
+
+    private void OnApplicationPause(bool pause)
+    {
+      
     }
 }
