f3fbba9887e98e1a1697e5a680812382d2a855e6
Shaun Brady <k2129184@kingston.ac.uk>
2023-10-23 16:56:09 +0100

    added game manager script
    added some functions within.

--- Bread TD/Assets/Scripts/GameManager.cs
+++ Bread TD/Assets/Scripts/GameManager.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    //values needed to be persistant accross scenes
+    static int Score;
+
+    static List<bool> lvlLockList; //just showes if a level is unlocked or not
+
+    //list of levels
+    
+
+    //lvl values
+    int Lives;
+    int lvlScore;
+    int wave;
+    int currentWave;
+    int waveDelay;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Lives = 3; lvlScore = 0; wave = 1;
+        StartWave(wave);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void LoadLevel()
+    {
+        //first check if level is locked
+
+        //need to add lvl score to total score
+    }
+
+    void StartWave(int waveNum)
+    {
+        //delay before wave starts
+    }
+
+    void FinishWave()
+    {
+        //make sure all enemies are dead before finishing
+    }
+
+    bool IsLevelLocked(int level) 
+    {
+        //check if specific level is locked
+        bool lvllocked = lvlLockList[level];
+        
+        return lvllocked;
     }
 }
