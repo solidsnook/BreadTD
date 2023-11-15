cebe8e8de2b00335727f1aa0a6e74ae588207158
Joshua Palmer <K2111154@kingston.ac.uk>
2023-11-15 18:07:31 +0000

    Revert "Merge branch 'main' into Josh"
    This reverts commit 9f2a9189d2a106a2a5766764cba348b1d5e0f678, reversing
    changes made to 5c7a1738f0e5b8ade769c74ccf7610df0bc3411b.

--- Bread TD/Assets/Scripts/TowerManager.cs
+++ Bread TD/Assets/Scripts/TowerManager.cs
@@ -9,8 +9,6 @@ public class TowerManager : MonoBehaviour
 
     public GameObject gameManager;
 
-    public GameObject SelectedButton;
-
     Vector2 CurrentButtonPos;
 
     public void Start()
@@ -21,18 +19,12 @@ public class TowerManager : MonoBehaviour
     //function will be called by button and whatever tower is chosen and passed tp this function will be placed
     public void TowerPlace(GameObject Tower)
     {
-        if (SelectedButton.GetComponent<ButtonPlacerScript>().IsOcupied())
-        {
-            Debug.Log("Cant PLace Tower Here Alrready Has Tower Placed");
-            return;
-        }
-
         int cost = Tower.GetComponent<TowerScript>().cost;
         if (gameManager.GetComponent<GameManager>().RemoveCrumbs(cost))
         {
             Instantiate(Tower, CurrentButtonPos, Quaternion.identity);
-            SelectedButton.GetComponent<ButtonPlacerScript>().SetOcupied(true);
             CloseBuyScreen();
+
         }
     }
 
@@ -40,8 +32,7 @@ public class TowerManager : MonoBehaviour
     {
         BuyScreenOBJ.SetActive(true);
 
-        SelectedButton = Button;
-        CurrentButtonPos = SelectedButton.transform.position;
+        CurrentButtonPos = Button.transform.position;
     }
 
     public void CloseBuyScreen()
