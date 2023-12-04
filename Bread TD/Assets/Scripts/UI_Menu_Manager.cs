36109a96db2a3766ac15b83e0351f5a3c3445f97
Ishaq Islam <K2131958@kingston.ac.uk>
2023-12-04 14:20:27 +0000

    Fixes

--- Bread TD/Assets/Scripts/UI_Menu_Manager.cs
+++ Bread TD/Assets/Scripts/UI_Menu_Manager.cs
@@ -30,49 +30,49 @@ public class UI_Menu_Manager : MonoBehaviour
         SettingsMenu.SetActive(false);
         TowersMenu.SetActive(false);
     }
-    
-    //private void RescaleCamera()
-    //{
- 
-    //    if (Screen.width == ScreenSizeX && Screen.height == ScreenSizeY) return;
- 
-    //    float targetaspect = 16.0f / 9.0f;
-    //    float windowaspect = (float)Screen.width / (float)Screen.height;
-    //    float scaleheight = windowaspect / targetaspect;
-    //    //Camera camera = GetComponent<Camera>();
- 
-    //    if (scaleheight < 1.0f)
-    //    {
-    //        Rect rect = camera.rect;
- 
-    //        rect.width = 1.0f;
-    //        rect.height = scaleheight;
-    //        rect.x = 0;
-    //        rect.y = (1.0f - scaleheight) / 2.0f;
- 
-    //        camera.rect = rect;
-    //    }
-    //    else // add pillarbox
-    //    {
-    //        float scalewidth = 1.0f / scaleheight;
- 
-    //        Rect rect = camera.rect;
- 
-    //        rect.width = scalewidth;
-    //        rect.height = 1.0f;
-    //        rect.x = (1.0f - scalewidth) / 2.0f;
-    //        rect.y = 0;
- 
-    //        camera.rect = rect;
-    //    }
- 
-    //    ScreenSizeX = Screen.width;
-    //    ScreenSizeY = Screen.height;
-    //}
+
+    private void RescaleCamera()
+    {
+
+        if (Screen.width == ScreenSizeX && Screen.height == ScreenSizeY) return;
+
+        float targetaspect = 16.0f / 9.0f;
+        float windowaspect = (float)Screen.width / (float)Screen.height;
+        float scaleheight = windowaspect / targetaspect;
+        //Camera camera = GetComponent<Camera>();
+
+        if (scaleheight < 1.0f)
+        {
+            Rect rect = camera.rect;
+
+            rect.width = 1.0f;
+            rect.height = scaleheight;
+            rect.x = 0;
+            rect.y = (1.0f - scaleheight) / 2.0f;
+
+            camera.rect = rect;
+        }
+        else // add pillarbox
+        {
+            float scalewidth = 1.0f / scaleheight;
+
+            Rect rect = camera.rect;
+
+            rect.width = scalewidth;
+            rect.height = 1.0f;
+            rect.x = (1.0f - scalewidth) / 2.0f;
+            rect.y = 0;
+
+            camera.rect = rect;
+        }
+
+        ScreenSizeX = Screen.width;
+        ScreenSizeY = Screen.height;
+    }
 
     private void Update()
     {
-        //RescaleCamera();
+        RescaleCamera();
     }
 
     public void PlayGame()
