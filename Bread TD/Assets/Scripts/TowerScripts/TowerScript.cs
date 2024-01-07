ac47d0322d993f7cafdd6a2d03c2da34d58e15a6
DankSaNiK4Life <kiddreece@hotmail.com>
2024-01-07 23:14:02 +0000

    Added Shot Amount and Shot Bar

--- Bread TD/Assets/Scripts/TowerScripts/TowerScript.cs
+++ Bread TD/Assets/Scripts/TowerScripts/TowerScript.cs
@@ -7,6 +7,7 @@ using UnityEditor;
 //using UnityEditor.Animations;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
+using UnityEngine.UI;
 
 public class TowerScript : MonoBehaviour
 {
@@ -15,26 +16,41 @@ public class TowerScript : MonoBehaviour
     public float speed;             // How fast the shot can move
     public float fireRate;          // How fast the tower can shoot
     public float attackRange;       // How far can the enemy be for the tower to shoot
-    public int cost;              // How much the tower costs to place down
-    public int sellValue;         // How much you can sell the tower for
+    public int cost;                // How much the tower costs to place down
+    public int sellValue;           // How much you can sell the tower for
     public int towerLevel;          // What tower level the tower is currently on
+    public int shotAmount;          // If the tower runs out of shots it is destroyed
     public GameObject Shot;         // Prefab for the Ketchup Shot
     public float aoe;               // How big the area of damage is 
     public int poisonAmount;        // How many times the damage is taken
     private GameObject enemy;
     private float time = 0f;
     public Animator animator;
+    public Slider shotBar;
+    public Gradient gradient;
+    public Image fill;
 
     //Audio Variable Declaration
     public AudioSource ShootSoundSource;
     public AudioClip ShootSoundEffect;
 
- 
+    //private TowerManager towerManager;
+    private GameObject CurrentButton;
+
+    void Start()
+    {
+        shotBar.maxValue = shotAmount;
+        fill.color = gradient.Evaluate(1f);
+        //towerManager.SelectedButton = CurrentButton;
+    }
 
     // Update is called once per frame
     void Update()
     {
         time = time + 1f * Time.deltaTime;
+
+        shotBar.value = shotAmount;
+        fill.color = gradient.Evaluate(shotBar.normalizedValue);
 
         // Updates the attackRange variable to the radius of the collider so it can be easily changed
         GetComponent<CircleCollider2D>().radius = attackRange;
@@ -48,29 +64,46 @@ public class TowerScript : MonoBehaviour
                 time = 0f;
             }
         }
+
+        if (shotAmount <= 0)
+        {
+            //set ocupied to false
+            //CurrentButton.GetComponent<ButtonPlacerScript>().SetOcupied(false);
+            //CurrentButton.GetComponent<Image>().sprite = towerManager.NotOccupied;
+            //CurrentButton.GetComponent<ButtonPlacerScript>().OcupiedTower = null;
+
+            //delete tower
+            //Destroy(this.gameObject);
+        }
     }
     void Shoot()
     {
-        // Position of Tower
-        float x = this.transform.position.x;
-        float y = this.transform.position.y;
+        // If the tower runs out of shots it is destroyed
+        if (shotAmount > 0)
+        {
+            // Position of Tower
+            float x = this.transform.position.x;
+            float y = this.transform.position.y;
 
-        // Play Shooting Animation
-        animator.SetTrigger("IsShooting?");
-        ShootSoundSource.PlayOneShot(ShootSoundEffect);
+            // Play Shooting Animation
+            animator.SetTrigger("IsShooting?");
+            ShootSoundSource.PlayOneShot(ShootSoundEffect);
 
-        // Spawn Ketchup Shot
-        GameObject currentShot = Instantiate(Shot, new Vector2(x, y + 1.2f), Quaternion.identity);
+            // Spawn Ketchup Shot
+            GameObject currentShot = Instantiate(Shot, new Vector2(x, y + 1.2f), Quaternion.identity);
 
-        // Adding new script to shot
-        ShotScript shot = currentShot.GetComponent<ShotScript>();
+            // Adding new script to shot
+            ShotScript shot = currentShot.GetComponent<ShotScript>();
 
-        // Save speed to new shot script
-        shot.shotSpeed = speed;
-        shot.Damage = damage;
-        shot.target = enemy;
-        shot.aoeRange = aoe;
-        shot.poisonAmount = poisonAmount;
+            // Save speed to new shot script
+            shot.shotSpeed = speed;
+            shot.Damage = damage;
+            shot.target = enemy;
+            shot.aoeRange = aoe;
+            shot.poisonAmount = poisonAmount;
+
+            shotAmount--;
+        }
     }
 
     void OnTriggerStay2D(Collider2D collision)
